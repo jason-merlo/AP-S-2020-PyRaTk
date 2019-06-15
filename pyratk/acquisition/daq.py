813d8e3bcd6a9e1cc64b94a819958b00e2878103
Jason Merlo <macman320@gmail.com>
2019-06-15 00:30:22 -0400

    Added motion datatypes for StateVector and StateMatrix

---
+++ pyratk/acquisition/daq.py
@@ -0,0 +1,183 @@
+# -*- coding: utf-8 -*-
+"""
+DAQ Manager class.
+
+Handles interfacing with physical DAQ and controls data flow from DAQ.
+
+TODO: Abstract base DAQ class from NI-DAQ class.
+
+Author: Jason Merlo
+Maintainer: Jason Merlo (merlojas@msu.edu)
+
+Dependencies: nidaqmx, random, threading, time, numpy
+"""
+try:
+    import nidaqmx              # Used for NI-DAQ hardware
+    from nidaqmx import stream_readers
+except ImportError:
+    print('Warning: nidaqmx module not imported')
+import threading                # Used for creating thread and sync events
+import time
+import numpy as np
+from pyratk.datatypes.ts_data import TimeSeries
+
+
+class DAQ(object):
+    def __init__(self, daq_type="NI-DAQ",
+                 sample_rate=44100, sample_size=4096,
+                 # NI-DAQ specific
+                 dev_string="Dev1/ai0:7",
+                 sample_mode=nidaqmx.constants.AcquisitionType.FINITE):
+        """
+        Create sampling task on DAQ and opens I & Q channels for radars.
+
+        arguments:
+        dev_string -- device and ports to initialize (default: "Dev1/ai0:7")
+        sample_rate -- frequency in Hz to sample at (default: 44100)
+        sample_mode -- finite or continuous acquisition (default: finite)
+        sample_size -- size of chunk to read (default/max: 4095)
+        """
+        # Copy member data
+        # General arguments
+        self.sample_rate = sample_rate
+        self.sample_size = sample_size
+        self.daq_type = daq_type
+        self.paused = False  # Start running by default
+        self.reset_flag = False
+
+        # Create sevent for controlling draw events only when there is new data
+        self.data_available = threading.Event()
+
+        self.sample_num = 0
+
+        # Device specific arguments
+        if self.daq_type == "NI-DAQ":
+            self.sample_mode = sample_mode
+            self.dev_string = dev_string
+            # Get number of channels to sample
+            if self.dev_string[-2] == ':':
+                self.num_channels = int(
+                    self.dev_string[-1]) - int(self.dev_string[-3]) + 1
+            else:
+                self.num_channels = int(self.dev_string[-1]) + 1
+
+            # Create new sampling task
+            try:
+                # Try to create sampling task
+                self.task = nidaqmx.Task()
+
+                self.task.ai_channels.add_ai_voltage_chan(dev_string)
+
+                self.task.timing.cfg_samp_clk_timing(
+                    sample_rate, sample_mode=sample_mode,
+                    samps_per_chan=sample_size)
+                self.in_stream = \
+                    stream_readers.AnalogMultiChannelReader(
+                        self.task.in_stream)
+            except nidaqmx._lib.DaqNotFoundError:
+                # On failure (ex. on mac/linux) generate random data for
+                # development purposes
+                # TODO: switch to PyDAQmx for mac/linux
+                # TODO: is there any reason to keep nidaqmx for windows?
+                # TODO: try performance comparison
+                self.daq_type = "FakeDAQ"
+                print("="*60)
+                print("Warning: Using fake data. nidaqmx is not "
+                      "supported on this platform.")
+                print("="*60)
+            except nidaqmx.errors.DaqError as e:
+                print(e)
+                self.daq_type = "FakeDAQ"
+                print("="*60)
+                print("Warning: Using fake data. DAQ could not be detected.")
+                print("="*60)
+
+        elif self.daq_type == "PyAudioDAQ":
+            pass  # TODO insert pyaudio support here
+
+        # Create data member to store samples
+        self.data = np.empty((self.num_channels, self.sample_size),)
+
+        # Spawn sampling thread
+        self.run()
+
+        # Create data buffers
+        self.buffer = []
+        length = 4096
+        shape = (self.num_channels, self.sample_size)
+        self.ts_buffer = TimeSeries(length, shape)
+
+    # === SAMPLING ======================================================
+    def sample_loop(self):
+        """Call get_samples forever."""
+        while self.running:
+            if self.paused:
+                time.sleep(0.1)  # sleep 100 ms
+            else:
+                self.get_samples()
+        print("Sampling thread stopped.")
+
+    def get_samples(self):
+        """Read device sample buffers returning the specified sample size."""
+        if self.daq_type == "FakeDAQ":
+            sleep_time = self.sample_size / self.sample_rate
+            self.data = np.random.randn(
+                self.num_channels, self.sample_size) * 0.001 + \
+                np.random.randn(1) * 0.001 + 0.01
+            time.sleep(sleep_time)
+        else:
+            try:
+                read_all = nidaqmx.constants.READ_ALL_AVAILABLE
+                self.in_stream.read_many_sample(
+                    self.data,
+                    number_of_samples_per_channel=read_all,
+                    timeout=1.0)
+
+                # print('received update')
+            except nidaqmx.errors.DaqError as err:
+                print("DAQ exception caught: {0}\n".format(err))
+
+        self.buffer.append((self.data, self.sample_num))
+        self.ts_buffer.append(self.data)
+
+        self.sample_num += 1
+        # Set the update event to True once data is read in
+        self.data_available.set()
+
+    # === CONTROL =======================================================
+    def close(self):
+        print("Stopping sampling thread...")
+        self.running = False
+        if self.daq_type == "NI-DAQ" and hasattr(self, 'task'):
+            self.task.close()  # Close nidaq gracefully
+        if self.t_sampling.is_alive():
+            try:
+                self.t_sampling.join()
+            except Exception as e:
+                print("Error closing sampling thread: ", e)
+
+    def run(self):
+        # Spawn sampling thread
+        self.running = True
+        self.t_sampling = threading.Thread(target=self.sample_loop)
+        try:
+            if not self.t_sampling.is_alive():
+                print('Staring sampling thread')
+                self.t_sampling.start()
+            self.paused = False
+        except RuntimeError as e:
+            print('Error starting sampling thread: ', e)
+
+    def pause(self):
+        self.paused = True
+
+    def reset(self):
+        self.buffer = []
+        self.ts_buffer.clear()
+        self.sample_num = 0
+        self.reset_flag = True
+
+    # === PROPERTIES ====================================================
+    @property
+    def type(self):
+        return self.daq_type
---
+++ pyratk/datatypes/motion.py
@@ -0,0 +1,210 @@
+"""
+Geometry Class.
+
+Author: Jason Merlo
+Maintainer: Jason Merlo (merlojas@msu.edu)
+"""
+import numpy as np
+
+
+class StateVector(object):
+    """1-Dimensional state vector object."""
+
+    def __init__(self, mat):
+        """Initialize vector."""
+        # Check for appropriate dimensionality and type
+        if type(mat) is not np.ndarray:
+            raise TypeError("Argument 'mat' must be of type numpy.ndarray.")
+        if mat.shape != (3,):
+            raise ValueError("Argument 'mat' must be of shape (3,).")
+
+        self.q = mat
+
+    def __repr__(self):
+        """Print StateVector to output stream."""
+        return 'pos: {:+6.3}\tvel: {:+6.3}\tacc: {:+6.3}'.format(
+            self.p, self.v, self.a)
+
+    def __getitem__(self, val):
+        return self.q[val]
+
+    @property
+    def p(self):
+        """Getter for positional element of state vector."""
+        return self.q[0]
+
+    @property
+    def v(self):
+        """Getter for velocity element of state vector."""
+        return self.q[1]
+
+    @property
+    def a(self):
+        """Getter for acceleration element of state vector."""
+        return self.q[2]
+
+    @p.setter
+    def p(self, val):
+        """Setter for positional element of state vector."""
+        self.q[0] = val
+
+    @v.setter
+    def v(self, val):
+        """Setter for velocity element of state vector."""
+        self.q[1] = val
+
+    @a.setter
+    def a(self, val):
+        """Setter for acceleration for element of state vector."""
+        self.q[2] = val
+
+
+class StateMatrix(object):
+    """3-Dimensional state matrix object."""
+
+    coordinate_types = {
+        'cartesian': ('x', 'y', 'z'),
+        'cylindrical': ('r', 'theta', 'z'),
+        'spherical': ('rho', 'theta', 'phi')
+    }
+
+    # Valid aliases used for q indexing
+    aliases = {
+        # q[0] aliases
+        'x': 'q0',
+        'r': 'q0',
+        'rho': 'q0',
+        # q[1] aliases
+        'y': 'q1',
+        'theta': 'q1',
+        # q[0] aliases
+        'z': 'q2',
+        'phi': 'q2'
+    }
+
+    def __init__(self, mat, coordinate_type='cartesian'):
+        """Initialize matrix and coordinate type."""
+        # Check coordinate type
+        if coordinate_type not in self.coordinate_types.keys():
+            raise ValueError("Coordinate type must be one of the following:",
+                             self.valid_coordinate_types)
+
+        self.coordinate_type = coordinate_type
+
+        # Check for appropriate dimensionality and type
+        if type(mat) is not np.ndarray:
+            raise TypeError("Argument 'mat' must be of type numpy.ndarray.")
+
+        if mat.shape != (3, 3):
+            raise ValueError("Argument 'mat' must be of shape (3, 3).")
+
+        self.q = mat
+
+    # === Alias handling === #
+    def __setattr__(self, name, value):
+        """Override set attribute method to handle aliases."""
+        name = self.aliases.get(name, name)
+        object.__setattr__(self, name, value)
+
+    def __getattr__(self, name):
+        """Override set attribute method to handle aliases and error check."""
+        if name == "aliases":
+            # recursively calles __getattr__ aliases
+            raise AttributeError
+
+        # Perform coordinate type checking
+        if self.coordinate_type == 'cartesian':
+            if name not in coordinate_types['cartesian']:
+                raise AttributeError("Cartesian coordinate has no attribute",
+                                     name)
+        elif self.coordinate_type == 'cylindrical':
+            if name not in coordinate_types['cylindrical']:
+                raise AttributeError("Cylindrical coordinate has no attribute",
+                                     name)
+        elif self.coordinate_type == 'spherical':
+            if name not in coordinate_types['spherical']:
+                raise AttributeError("Spherical coordinate has no attribute",
+                                     name)
+
+        name = self.aliases.get(name, name)
+        return object.__getattribute__(self, name)
+
+    def __repr__(self):
+        """Print StateVector to output stream."""
+        # if self.coordinate_type == 'cartesian':
+        #     ret_str = '\tx: {:}\n\ty: {:}\n\tz: {:}'.format(
+        #         self.x, self.y, self.z)
+        # elif self.coordinate_type == 'cylindrical':
+        #     ret_str = 'r: {:}\ntheta: {:}\nz: {:}'.format(
+        #         self.r, self.theta, self.z)
+        # elif self.coordinate_type == 'spherical':
+        #     ret_str = 'rho: {:}\ntheta: {:}\nphi: {:}'.format(
+        #         self.rho, self.theta, self.z)
+        if self.coordinate_type == 'cartesian':
+            coord_list = self.coordinate_types['cartesian']
+        elif self.coordinate_type == 'cylindrical':
+            coord_list = self.coordinate_types['cylindrical']
+        elif self.coordinate_type == 'spherical':
+            coord_list = self.coordinate_types['spherical']
+
+        ret_str = '\n' + ' '*6 + '{:^7}{:^7}{:^7}'.format(*coord_list)
+
+        # loop through all indexes in each state vector
+        derivative_list = ('pos', 'vel', 'acc')
+        for i in range(3):
+            ret_str += '\n{:>6}{:+7.3}{:+7.3}{:+7.3}'.format(
+                derivative_list[i], self.x[i], self.y[i], self.z[i]
+            )
+        return ret_str + '\n'
+
+    # State matrix attributes
+    @property
+    def x(self):
+        """First axis of state matrix, q[0]."""
+        return StateVector(self.q[0])
+
+    @property
+    def y(self):
+        """Second axis of state matrix, q[1]."""
+        return StateVector(self.q[1])
+
+    @property
+    def z(self):
+        """Third axis of state matrix, q[2]."""
+        return StateVector(self.q[2])
+
+    @x.setter
+    def x(self, state_vec):
+        """Setter for first axis of state matrix, q[0]."""
+        state_type = type(state_vec)
+        if state_type is np.ndarray:
+            self.q[0] = state_vec
+        elif state_type is StateVector:
+            self.q[0] = state_vec.q
+        else:
+            raise TypeError("Argument must be ndarray of shape (3,) or"
+                            " StateVector")
+
+    @y.setter
+    def y(self, state_vec):
+        """Setter for second axis of state matrix, q[1]."""
+        state_type = type(state_vec)
+        if state_type is np.ndarray:
+            self.q[1] = state_vec
+        elif state_type is StateVector:
+            self.q[1] = state_vec.q
+        else:
+            raise TypeError("Argument must be ndarray of shape (3,) or"
+                            " StateVector")
+
+    @z.setter
+    def z(self, state_vec):
+        """Setter for third axis of state matrix, q[2]."""
+        state_type = type(state_vec)
+        if state_type is np.ndarray:
+            self.q[2] = state_vec
+        elif state_type is StateVector:
+            self.q[2] = state_vec.q
+        else:
+            raise TypeError("Argument must be ndarray of shape (3,) or"
+                            " StateVector")
